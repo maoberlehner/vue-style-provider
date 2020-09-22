4e7e40a94335e8ee560262978abea25ed3d9c5b6
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-09-22 19:18:24 +0200

    Make styled props styles reusable

--- src/components/ProvideStylePadding.vue
+++ src/components/ProvideStylePadding.vue
@@ -43,45 +43,9 @@ export default {
 </script>
 
 <style lang="scss" module>
+@import '../composables/styled-props';
+
 .root {
-  --padding-bp-s: var(--padding);
-  --padding-bp-m: var(--padding-bp-s);
-  --padding-bp-l: var(--padding-bp-m);
-
-  padding:
-    var(--padding-top, var(--padding))
-    var(--padding-right, var(--padding))
-    var(--padding-bottom, var(--padding))
-    var(--padding-left, var(--padding));
-
-  --x-padding-bp-s:
-    var(--padding-top-bp-s, var(--padding-top, var(--padding-bp-s)))
-    var(--padding-right-bp-s, var(--padding-right, var(--padding-bp-s)))
-    var(--padding-bottom-bp-s, var(--padding-bottom, var(--padding-bp-s)))
-    var(--padding-left-bp-s, var(--padding-left, var(--padding-bp-s)));
-
-  @media (min-width: 376px) {
-    padding: var(--x-padding-bp-s);
-  }
-
-  --x-padding-bp-m:
-    var(--padding-top-bp-m, var(--padding-top-bp-s, var(--padding-bp-m)))
-    var(--padding-right-bp-m, var(--padding-right-bp-s, var(--padding-bp-m)))
-    var(--padding-bottom-bp-m, var(--padding-bottom-bp-s, var(--padding-bp-m)))
-    var(--padding-left-bp-m, var(--padding-left-bp-s, var(--padding-bp-m)));
-
-  @media (min-width: 768px) {
-    padding: var(--x-padding-bp-m);
-  }
-
-  --x-padding-bp-l:
-    var(--padding-top-bp-l, var(--padding-top-bp-m, var(--padding-bp-l)))
-    var(--padding-right-bp-l, var(--padding-right-bp-m, var(--padding-bp-l)))
-    var(--padding-bottom-bp-l, var(--padding-bottom-bp-m, var(--padding-bp-l)))
-    var(--padding-left-bp-l, var(--padding-left-bp-m, var(--padding-bp-l)));
-
-  @media (min-width: 1024px) {
-    padding: var(--x-padding-bp-l)
-  }
+  @include use-styled-props('padding');
 }
 </style>
