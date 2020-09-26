22302a70699fda0a21913a2efebe2181deed7cea
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-09-26 18:47:20 +0200

    Add margin style provider

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,29 @@
         Hello World
       </div>
     </ProvideStylePadding>
+    <ProvideStyleMargin
+      v-slot="{ $$margin }"
+      :margin="[`m`, `l@s`, `xl@l`]"
+      :margin-top="[`xl@m`]"
+    >
+      <div
+        :class="$$margin.class"
+        :style="$$margin.style"
+      >
+        Hello World
+      </div>
+    </ProvideStyleMargin>
   </div>
 </template>
 
 <script>
+import ProvideStyleMargin from './components/ProvideStyleMargin.vue';
 import ProvideStylePadding from './components/ProvideStylePadding.vue';
 
 export default {
   name: `App`,
   components: {
+    ProvideStyleMargin,
     ProvideStylePadding,
   },
 };
---
+++ src/components/ProvideStyleMargin.vue
@@ -0,0 +1,51 @@
+<template>
+  <slot
+    :$$margin="{
+      class: $style.root,
+      style,
+    }"
+  />
+</template>
+
+<script>
+import {
+  sides,
+  spacings,
+} from '../style.config.json';
+import {
+  generateComponentProps,
+  useStyledProps,
+} from '../composables/styled-props';
+
+let cssProperty = `margin`;
+let variants = sides;
+
+export default {
+  name: `ProvideStyleMargin`,
+  props: generateComponentProps({
+    cssProperty,
+    variants,
+  }),
+  setup(props) {
+    let { style } = useStyledProps({
+      cssProperty,
+      defaultOption: spacings.m,
+      options: spacings,
+      props,
+      variants,
+    });
+
+    return {
+      style,
+    };
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import '../composables/styled-props';
+
+.root {
+  @include use-styled-props('margin');
+}
+</style>
